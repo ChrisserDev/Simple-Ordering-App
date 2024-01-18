d8c91318826983b49601fe60f339e533400d05e6
ChrisserDev <126911205+ChrisserDev@users.noreply.github.com>
2024-01-18 17:54:21 +0000

    Project finished

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+//Importing the menuArray that it's being used.
+
 import { menuArray } from "./data.js";
 
+//Initializing an empty array for the ordered items by default.
 let orderItems = [];
 
+//Function that renders the items from the data array and displays them on the page.
 function getMenu() {
     let menuItems = "";
 
@@ -11,7 +15,7 @@ function getMenu() {
                 <img src="${item.image}" class="item-img">
                 <div class="menu-details">
                     <h3>${item.name}</h3>
-                    <p>${item.ingredients}</p>
+                    <p><strong>Ingredients:</strong> ${item.ingredients}</p>
                     <span>£${item.price}</span>
                 </div>
                 <i class="fa-solid fa-circle-plus" data-order="${item.id}"></i>
@@ -29,14 +33,16 @@ document.addEventListener("click", function (e) {
     }
 });
 
+//Function that will check if the id of the item clicked is the same as the id of the item in the array.
 function handleOrderClick(itemId) {
     
     const menu = menuArray.find((item) => item.id == itemId, 0);
     orderItems.push(menu);
 
     renderOrder();
-}
+};
 
+//Function that will render an item to the customer order once it's been clicked.
 function renderOrder() {
 
     let orderItemHtml = orderItems.map(function(item) {
@@ -66,7 +72,7 @@ function renderOrder() {
         button.addEventListener("click", () => removeItem(button.getAttribute("data-item-id")))
     );
         
-    // Add event listener for "Complete Order" button
+    // Event listener for "complete order" button
     const completeOrderBtn = document.getElementById("complete-order-btn");
         
     completeOrderBtn.addEventListener("click", function () {
@@ -75,6 +81,7 @@ function renderOrder() {
     });
 };
 
+//Function that will remove an item from the orderItems array once it's been triggered.
 function removeItem(itemIdToRemove) {
     orderItems = orderItems.filter(item => item.id != itemIdToRemove);
     
@@ -94,6 +101,8 @@ const name = document.getElementById("card-name");
 const cardNumber = document.getElementById("card-number");
 const cardCVV = document.getElementById("card-cvv");
 
+
+//Event Handling for the Payment Form
 paymentForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
@@ -103,18 +112,20 @@ paymentForm.addEventListener("submit", function (e) {
     confirmationMessage.style.display = "flex";
     document.getElementById("customer-order").style.display = "none";
     
+    // Rendering a confirmation message for 3 seconds.
     setTimeout(() => {
         confirmationMessage.textContent = "";
         confirmationMessage.style.display = "none";
     }, 3000);
     
+    //Reseting the values after the form has been submitted.
     name.value = "";
     cardNumber.value = "";
     cardCVV.value = "";
     orderItems = [];
 });
 
-
+//Event handling for the modal
 document.addEventListener("click", function (e) {
     // Check if the clicked element is outside the modal
     if (!modalContainer.contains(e.target) && e.target.id !== "complete-order-btn") {
